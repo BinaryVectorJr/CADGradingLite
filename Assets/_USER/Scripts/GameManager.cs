0106981b51057a7924fe1d7927cc6688ecfb2bf6
Moz <amazumda@purdue.edu>
2025-01-02 18:51:45 -0800

    Updated GameManager to DontDestroyOnLoad
    Added button manager, where listeners are added to each button dynamically. Also based on the string parameter of the listener, functions have been added that dynamically execute on specific gameobjects based on their names - which gets searched in the dynamically generated List when the scene starts.

--- Assets/_USER/Scripts/GameManager.cs
+++ Assets/_USER/Scripts/GameManager.cs
@@ -8,33 +8,48 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager gmInstance {get; private set;}
+    public enum GameState
+    {
+        MAIN_MENU = 0,
+        REGULAR_GRADING = 1,
+        PROJECT_GRADING = 2,
+        LOAD_FILES = 3
+    };
 
-    [SerializeField] 
-    private List<SceneAsset> scenesList;
+    public static GameManager gmInstance;
 
-    [SerializeField]
-    private GameObject[] modalPanels;
+    public GameState currentState = GameState.MAIN_MENU;
 
-    [SerializeField]
-    private GameObject[] buttonTexts;
+    public List<SceneAsset> scenesList;
+
+    public List<GameObject> modalPanels = new List<GameObject>();
+    public List<Button> allButtons = new List<Button>();
+
+    public string tagToTrack = "Modal";
+
 
     void Awake()
     {
         if(gmInstance != null && gmInstance != this)
         {
-            Destroy(this);
+            Destroy(gameObject);
+            return;
         }
         else
         {
             gmInstance = this;
+            currentState = GameState.MAIN_MENU;
+            DontDestroyOnLoad(gameObject);
         }
+
+        // Add listener to when scene is loaded
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -43,10 +58,67 @@ public class GameManager : MonoBehaviour
         
     }
 
+    // When scene is loaded, execute function
+    void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, LoadSceneMode mode)
+    {
+        LoadPanels();
+        LoadButtons();
+    }
+    
+    // Custom function to find panel gameobjects even if they are hidden
+    void LoadPanels()
+    {
+        modalPanels.Clear();
+        GameObject[] allPanels = GameObject.FindObjectsOfType<GameObject>(true);
+        foreach(GameObject go in allPanels)
+        {
+            if(go.CompareTag(tagToTrack))
+            {
+                modalPanels.Add(go);
+            }
+        }
+
+    }
+
+    void LoadButtons()
+    {
+        allButtons.Clear();
+        Button[] _allButtons = GameObject.FindObjectsOfType<Button>(true);
+        foreach(Button btn_go in _allButtons)
+        {
+            allButtons.Add(btn_go);
+            btn_go.onClick.AddListener(() => OnButtonClick(btn_go));
+        }
+    }
+
+    public void UpdateTrackedObjects()
+    {
+        LoadPanels();
+        LoadButtons();
+    }
+
     public void SwitchScenes(int _sceneNumber)
     {
         SceneManager.LoadScene(gmInstance.scenesList[_sceneNumber].name.ToString());
-        modalPanels = GameObject.FindGameObjectsWithTag("Modal");
+        switch(_sceneNumber)
+        {
+            case 0:
+                currentState = GameState.MAIN_MENU;
+            break;
+
+            case 1:
+                currentState = GameState.REGULAR_GRADING;
+            break;
+
+            case 3:
+                currentState = GameState.PROJECT_GRADING;
+            break;
+
+            default:
+                currentState = GameState.MAIN_MENU;
+            break;
+        }
+        LoadPanels();
         //print(scenesList[sceneNumber].name.ToString());
     }
 
@@ -75,7 +147,41 @@ public class GameManager : MonoBehaviour
         modalPanels[_modalNumber].gameObject.SetActive(false);
     }
 
-    public void Quit()
+    void OnButtonClick(Button _btn)
+    {
+        if(_btn.transform.name == "BTN_Return")
+        {
+            OnSceneChangeClick("00StartScene");
+        }
+        if(_btn.transform.name == "BTN_RegularGrading")
+        {
+            OnSceneChangeClick("01GradingScene");;
+        }
+        if(_btn.transform.name == "BTN_Quit")
+        {
+            OnQuitButtonClick();
+        }
+        if(_btn.transform.name == "BTN_ChangeAssign")
+        {
+            OnPanelChangeClick("PNL_AssignmentSelector");
+        }
+    }
+
+    void OnSceneChangeClick(string _sceneName)
+    {
+        // Use FindIndex to find the index based on the name
+        int index = scenesList.FindIndex(scene => scene.name.Equals(_sceneName));
+        SwitchScenes(index);
+    }
+
+    void OnPanelChangeClick(string _panelName)
+    {
+        // Use FindIndex to find the index based on the name
+        int index = modalPanels.FindIndex(scene => scene.name.Equals(_panelName));
+        PersistShowModals(index);
+    }
+
+    void OnQuitButtonClick()
     {
         Application.Quit();
     }
